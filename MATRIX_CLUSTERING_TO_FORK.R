5ccc4a137319fc84d2b14d346d52576899371dba
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-09-24 19:04:24 +0800

    Update MATRIX_CLUSTERING_TO_FORK.R

--- MATRIX_CLUSTERING_TO_FORK.R
+++ MATRIX_CLUSTERING_TO_FORK.R
@@ -3,3 +3,86 @@ cebpa <- loadPeaks(id = "MM1_HSA_HepG2_CEBPA", includeMotifOnly = TRUE)
 cebpa[,2] <- cebpa[,2]-50
 cebpa[,3] <- cebpa[,3]+50
 write.table(cebpa[,1:3],"HepG2_CEBPA.bed",sep="\t",quote=F,row.names=F,col.names=F)
+################################################################################
+################################################################################
+################################################################################
+# Example 1 MAFF K562
+FPWM_MAFF <- createFPWM(mainTF ="MAFF",
+                            partners = c("MAFK","NFE2"),
+                                cell = "K562", 
+                        forkPosition = 7)
+
+write.FPWM( FPWM = FPWM_MAFF, 
+            format = "transfac", 
+            fileName = "FPWM_Counts_MAFF+MAFK_NFE2+K562.transfac")
+
+FPWM_MAFF_p <- createFPWM(mainTF ="MAFF",
+                            partners = c("MAFK","NFE2"),
+                                cell = "K562", 
+                        forkPosition = 7,
+                       probabilityMatrix = TRUE)
+
+write.FPWM( FPWM = FPWM_MAFF_p, 
+            format = "transfac", 
+            fileName = "FPWM_Probability_MAFF+MAFK_NFE2+K562.transfac")
+
+MAFF_MAFK_K562 <- commonPeaks(target_peak_id="MM1_HSA_K562_MAFF",
+                                       motif_only_for_target_peak=TRUE,
+                                       compared_peak_id="MM1_HSA_K562_MAFK",
+                                       motif_only_for_compared_peak=TRUE,
+                                       methylation_profile_in_narrow_region=FALSE)
+MAFF_MAFK_K562_peaks <-MAFF_MAFK_K562[,1][[1]]@common_peak # 9790
+write.table(MAFF_MAFK_K562_peaks[,1:3],"MAFF_MAFK_K562_9790peaks.bed",sep="\t",quote=F,row.names=F,col.names=F)
+
+
+MAFF_NFE2_K562 <- commonPeaks(target_peak_id="MM1_HSA_K562_MAFF",
+                                       motif_only_for_target_peak=TRUE,
+                                       compared_peak_id="MM1_HSA_K562_NFE2",
+                                       motif_only_for_compared_peak=TRUE,
+                                       methylation_profile_in_narrow_region=FALSE)
+MAFF_NFE2_K562_peaks <-MAFF_NFE2_K562[,1][[1]]@common_peak # 4821
+write.table(MAFF_NFE2_K562_peaks[,1:3],"MAFF_NEF2_K562_4821peaks.bed",sep="\t",quote=F,row.names=F,col.names=F)
+
+
+
+
+
+# Example 2 JUND HepG2
+FPWM_JUND <- createFPWM(mainTF ="JUND",
+                            partners = c("ATF2","FOSL2"),
+                                cell = "HepG2", 
+                        forkPosition = 6)
+
+write.FPWM( FPWM = FPWM_JUND, 
+            format = "transfac", 
+            fileName = "FPWM_Counts_JUND+ATF2_FOSL2+HepG2.transfac")
+
+FPWM_JUND_p <- createFPWM(mainTF ="JUND",
+                            partners = c("ATF2","FOSL2"),
+                                cell = "HepG2", 
+                        forkPosition = 6,
+                       probabilityMatrix = TRUE)
+
+write.FPWM( FPWM = FPWM_JUND_p, 
+            format = "transfac", 
+            fileName = "FPWM_Probability_JUND+ATF2_FOSL2+HepG2.transfac")
+
+
+JUND_ATF2_HepG2 <- commonPeaks(target_peak_id="MM1_HSA_HepG2_JUND",
+                                       motif_only_for_target_peak=TRUE,
+                                       compared_peak_id="MM1_HSA_HepG2_ATF2",
+                                       motif_only_for_compared_peak=TRUE,
+                                       methylation_profile_in_narrow_region=FALSE)
+JUND_ATF2_HepG2_peaks <-JUND_ATF2_HepG2[,1][[1]]@common_peak # 6325
+write.table(JUND_ATF2_HepG2_peaks[,1:3],"JUND_ATF2_HepG2_6325peaks.bed",sep="\t",quote=F,row.names=F,col.names=F)
+
+
+JUND_FOSL2_HepG2 <- commonPeaks(target_peak_id="MM1_HSA_HepG2_JUND",
+                                       motif_only_for_target_peak=TRUE,
+                                       compared_peak_id="MM1_HSA_HepG2_FOSL2",
+                                       motif_only_for_compared_peak=TRUE,
+                                       methylation_profile_in_narrow_region=FALSE)
+JUND_FOSL2_HepG2_peaks <-JUND_FOSL2_HepG2[,1][[1]]@common_peak # 3160
+write.table(JUND_FOSL2_HepG2_peaks[,1:3],"JUND_FOSL2_HepG2_3160peaks.bed",sep="\t",quote=F,row.names=F,col.names=F)
+
+
