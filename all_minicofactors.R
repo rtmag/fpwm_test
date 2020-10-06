cc26200fd7ed63b1c768067b8a113d273634985d
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-10-06 17:29:29 +0800

    Update all_minicofactors.R

--- all_minicofactors.R
+++ all_minicofactors.R
@@ -4,7 +4,7 @@ all_record <- dataBrowser()
 
 mm_table <- all_record[all_record$source=="MethMotif",]
 
-for( i in 184:dim(mm_table)[1] ){
+for( i in 555:dim(mm_table)[1] ){
   miniCofactorReport( TF = mm_table$TF[i], cell = mm_table$cell_tissue_name[i],cobinding_threshold=.1 )
 }
 
