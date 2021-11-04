a40cc1f52b1352d84cd8dedee43a6fa4d95b6965
Roberto Tirado Magallanes <rtmag4@gmail.com>
2021-11-04 23:51:13 +0800

    Update quyInstructions2packDapack.R

--- quyInstructions2packDapack.R
+++ quyInstructions2packDapack.R
@@ -10,10 +10,21 @@ git commit -m "FPWM initial commit."
 git pull
 git push
 
-
+----------------------------------------------------------
 git init
 git remote add origin https://github.com/rtmag/forkedTF.git
 git add .
 git commit -m "forkedTF initial commit."
 git pull
 git push
+----------------------------------------------------------
+# 4/11/2021
+git init
+git remote add origin https://github.com/rtmag/forkedTF.git
+git add .
+git commit -m "forkedTF add pval-capabilities."
+git pull
+git push
+
+
+
